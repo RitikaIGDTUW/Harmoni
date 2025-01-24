843f6763475758dd604fe84a90b64e9e0ef573fc
ritika <ritikash06@gmail.com>
2025-01-24 20:02:15 +0530

    socket update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider=({children})=>{
     useEffect(()=>{
         if(authUser){
            
-            const socket=io("http://localhost:8080",{
+            const socket=io("https://harmoni-floi.onrender.com/",{
                 query:{
                     userId:authUser._id,
                 }
