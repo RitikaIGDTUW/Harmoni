bc2d7ce46e2261cb16f66f966a9223c6eab3e647
ritika <ritikash06@gmail.com>
2025-01-24 20:17:14 +0530

    flex change

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,11 +4,11 @@ import MessageContainer from '../../components/messages/MessageContainer'
 
 const Home = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding
-    backdrop-filter backdrop-blur-md bg-opacity-10'>
-        <Sidebar/>
-        <MessageContainer/>
-    </div>
+    
+    <div className='flex flex-col md:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10'>
+    <Sidebar/>
+    <MessageContainer/>
+</div>
   )
 }
 
